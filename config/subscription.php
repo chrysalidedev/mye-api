f616cc4fc89eb815a291e1dcfcb2bb24496c87a6
Jumeaux07 <zouzouacedric17@gmail.com>
2026-03-15 02:59:59 +0000

    add ket cnetpay

--- config/subscription.php
+++ config/subscription.php
@@ -5,9 +5,7 @@ return [
     'yearly_price'  => env('SUBSCRIPTION_YEARLY_PRICE', 9000),
 
     'cinetpay' => [
-        'apikey'  => env('91988269679b43217f5bb6.89080309'),
-        'site_id' => env('105886764'),
-        // 'apikey'  => env('CINETPAY_API_KEY'),
-        // 'site_id' => env('CINETPAY_SITE_ID'),
+        'apikey'  => env('CINETPAY_API_KEY'),
+        'site_id' => env('CINETPAY_SITE_ID'),
     ],
 ];
